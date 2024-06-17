9a0c0f8f92d6ac416102b3d1b7ce7a0d31a18ce3
Iqra Khaliq <iqra44938@gmail.com>
2024-06-17 04:35:59 -0700

    //

--- iqra-portfolio/src/components/FrontendLayout/FrontendLayout.jsx
+++ iqra-portfolio/src/components/FrontendLayout/FrontendLayout.jsx
@@ -1,14 +1,16 @@
 import { Collapse, Menu, Layout, Button } from 'antd';
 import { useState } from 'react';
 import { HomeOutlined, UserOutlined, MenuOutlined } from '@ant-design/icons';
-// import "./FrontendLayout.css";
+import "./FrontendLayout.css";
 const { Header } = Layout;
 const { Panel } = Collapse;
 
 const items = [
   { key: '1',  label: 'Home' },
-  { key: '2',  label: 'Page' },
-  { key: '3',  label: 'Icon' },
+  { key: '2',  label: 'About' },
+  { key: '3',  label: 'Portfolio' },
+  { key: '4',  label: 'Resume' },
+  { key: '5',  label: 'Contact' },
 ];
 
 const FrontendLayout = () => {
@@ -24,13 +26,17 @@ const FrontendLayout = () => {
   };
 
   return (
-    <Layout  className="h-screen  " style={{backgroundColor:"#444c58"}}>
-      <Header className="header flex   bg-gray-400" >
+    <Layout  className="h-screen main " style={{}}>
+      <Header className="header   bg-black h-[70px] w-[100%] m-auto" >
+
+        <div className='flex justify-between w-[80%]  m-auto'>
+
+      <span className='text-white mt-3 text-3xl'>Iqra_</span>
             
-            <Menu  mode="horizontal"  selectedKeys={selectedKeys} // Pass selectedKeys prop to activate the corresponding menu item
-      onClick={handleClick}  className="">
+            <Menu    mode="horizontal"  style={{padding:'5px'}} selectedKeys={selectedKeys} 
+      onClick={handleClick}  className="h-14 text-base  flex justify-end  w-[70%]">
               {items.map(item => (
-                <Menu.Item  key={item.key} icon={item.icon}  className={`animation ${selectedKeys.includes(item.key) ? 'selected' : ''}`}>
+                <Menu.Item  key={item.key} icon={item.icon}  className={`animation ${selectedKeys.includes(item.key) ? 'selected' : ''}  `}>
                   {item.label}
                 </Menu.Item>
               ))}
@@ -41,7 +47,24 @@ const FrontendLayout = () => {
               onClick={handleCollapse}
               className="toggle-btn lg:hidden"
             />
+              </div>
       </Header>
+
+    <Layout className=' second w-[75%] m-auto bg-black mt-5 text-white' >
+      <div className='p-10'>
+
+<h1 className='text-5xl  mt-8'>
+  Hi_
+</h1>
+
+<h1 className='text-5xl mt-5 '>
+I'M
+ <span className='text-12xl ml-4'>
+   IQRA A.KHALID
+  </span>
+   </h1>
+      </div>
+    </Layout>
     </Layout>
   );
 };
