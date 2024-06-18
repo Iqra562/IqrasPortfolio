73002a360d61253340f267cae4536f0327e62197
Iqra Khaliq <iqra44938@gmail.com>
2024-06-18 05:05:13 -0700

    added library css-transition-group

--- iqra-portfolio/src/App.jsx
+++ iqra-portfolio/src/App.jsx
@@ -1,7 +1,44 @@
-import FrontendLayout from "./components/FrontendLayout/FrontendLayout";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import HomeComponent from './pages/Home';
+import ContactComponent from './pages/Contact';
+import NavBar from './pages/Navbar';
+import './App.css';
 
-export default function App() {
+const App = () => {
   return (
-   <FrontendLayout/>
-  )
-}
+    <Router>
+      <div className="container">
+        <NavBar />
+        <RoutesWithTransitions />
+      </div>
+    </Router>
+  );
+};
+
+const RoutesWithTransitions = () => {
+  const location = useLocation();
+
+  return (
+    <div className="route-container">
+      <TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          timeout={300}
+          classNames="slide-up"
+        >
+            {/* <div className="route-section"> */}
+              
+          <Routes location={location}>
+            <Route exact path="/" element={<HomeComponent />} />
+            <Route path="/contact" element={<ContactComponent />} />
+          </Routes>
+            {/* </div> */}
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  );
+};
+
+export default App;
---
+++ iqra-portfolio/src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContactComponent = () => (
+  <div className="component">
+    <h1>Contact Page</h1>
+  </div>
+);
+
+export default ContactComponent;
