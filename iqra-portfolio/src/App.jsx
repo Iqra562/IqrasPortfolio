1adcdf3ddd3b6a6a046da168ac38d758d7c27031
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-07-11 00:40:55 +0500

    Update App.jsx

--- iqra-portfolio/src/App.jsx
+++ iqra-portfolio/src/App.jsx
@@ -7,7 +7,7 @@ import Contact from "./pages/Contact/Contact";
 import usePageTracking from './hooks/usePageTracking';
 
 function App() {
-    usePageTracking();
+//    usePageTracking();
   return (
    
         <Routes>
