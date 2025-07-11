b6556cecf64ae9954bd96bdc54d8e8dde57f0bc9
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-07-11 16:47:58 +0500

    update

--- iqra-portfolio/src/App.jsx
+++ iqra-portfolio/src/App.jsx
@@ -5,17 +5,16 @@ import Projects from "./pages/Projects/Projects";
 import Skills from "./pages/Skills/Skills";
 import Contact from "./pages/Contact/Contact";
 import usePageTracking from './hooks/usePageTracking';
-  import ReactGA from 'react-ga';
+import ReactGA from "react-ga4";
 import { useEffect } from "react";
-// ReactGA.initialize('G-1TSD80KJ1L');
 
 function App() {
     const location = useLocation();
     // usePageTracking();
 
-  // useEffect(() => {
-  //   ReactGA.send({ hitType: "pageview", page: location.pathname });
-  // }, [location]);
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
 
 
    
--- iqra-portfolio/src/main.jsx
+++ iqra-portfolio/src/main.jsx
@@ -5,7 +5,8 @@ import './index.css'
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./ThemeProvider/AppThemeProvider";
 import { BrowserRouter } from 'react-router-dom';
-
+import ReactGA from "react-ga4";
+ReactGA.initialize('G-1TSD80KJ1L');
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <ThemeProvider theme={theme}>
